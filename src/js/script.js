466b38fd7f3b422f6efcba1415a973cc8cf3cb00
Nazanin-fth <nazaninfatahinia@gmail.com>
2025-07-15 17:11:07 +0330

    Filter Box

--- src/js/script.js
+++ src/js/script.js
@@ -53,3 +53,65 @@ overlay.addEventListener("click", () => {
   menuToggle.checked = false;
   overlay.classList.add("hidden");
 });
+// Datepicker Font Setting
+function setDatepickerFont() {
+  setTimeout(function () {
+    document
+      .querySelectorAll(
+        ".pwt-datepicker, .pwt-datepicker *, .datepicker-plot-area"
+      )
+      .forEach(function (el) {
+        el.style.fontFamily =
+          "estedad-regular, estedad-medium, estedad-bold, sans-serif";
+        el.style.borderRadius = "12px";
+      });
+
+    //Hide switch calendar button
+    document.querySelectorAll(".pwt-btn-calendar").forEach(function (el) {
+      el.style.display = "none";
+    });
+  }, 100);
+}
+//my Datepicker script
+$(document).ready(function () {
+  $("#my-datepicker, #my-datepicker-2, #mobile-datepicker").persianDatepicker({
+    format: "YYYY/MM/DD",
+    initialValue: true,
+    responsive: true,
+    autoClose: true,
+    navigator: {
+      scroll: {
+        enabled: false,
+      },
+    },
+    onShow: setDatepickerFont,
+  });
+});
+// Time Picker script
+$(document).ready(function () {
+  $(
+    "#only-time-picker, #only-time-picker-2, #mobile-time-picker"
+  ).persianDatepicker({
+    format: "HH:mm",
+    initialValue: true,
+    responsive: true,
+    autoClose: true,
+    onlyTimePicker: true,
+    timePicker: {
+      enabled: true,
+      second: {
+        enabled: false,
+      },
+    },
+    navigator: {
+      scroll: {
+        enabled: false,
+      },
+    },
+    onShow: setDatepickerFont,
+  });
+});
+// Reapply datepicker font on navigation buttons click
+$(document).on("click", ".pwt-btn-next, .pwt-btn-prev", function () {
+  setTimeout(setDatepickerFont, 50);
+});
